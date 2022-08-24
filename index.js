4c3c38dac6f67db7c9b070dcddfe8844cd966441
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 21:35:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-let saveEl = document.getElementById("save-el")
-let ordersEl = document.getElementById("orders-el")
+
 let countEl = document.getElementById("count-el")
 let count = 0
+let saveEl = document.getElementById("save-el")
+let canceledEl = document.getElementById("canceled-el")
+let ordersTotal = document.getElementById("orders-total)
 
 function increment() {
     count += 1
@@ -17,11 +19,12 @@ function save() {
     let countStr = count + " , "
     let countStrDe = count - " , "
     saveEl.textContent += countStr
-    ordersEl.textContent -= countStrDe
+    canceledEl.textContent -= countStrDe
     countEl.textContent = 0
     count = 0
 }
 
 function orders() {
- let ordersEl = increment + decrement   
+ let ordersEl = increment + decrement 
+ saveEl.textContent +-= countStr
 }

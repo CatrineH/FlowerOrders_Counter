97b5c67d18e0d66a2edf6f6704b7985babc61461
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 21:53:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let countEl = document.getElementById("count-el")
 let count = 0
 let saveEl = document.getElementById("save-el")
 let canceledEl = document.getElementById("canceled-el")
-let ordersTotal = document.getElementById("orders-total")
+let ordersTotal = document.getElementById("orders").innerHTML = "Total Amount of flower orders today is: " + ordersTotal
 
 function increment() {
     count += 1
@@ -24,7 +24,7 @@ function save() {
     count = 0
 }
 
-function orders() {
+/*function orders() {
     let ordersTotal = countEl + canceledEl
    document.getElementById("orders").innerHTML = "Total Amount of flower orders today is: " + ordersTotal; 
-    }
+    }*/

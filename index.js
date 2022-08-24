fc5188f3b36639262e60a4b7a5e270b4513d7bba
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 21:44:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let countEl = document.getElementById("count-el")
 let count = 0
 let saveEl = document.getElementById("save-el")
 let canceledEl = document.getElementById("canceled-el")
-let ordersTotal = document.getElementById("orders-total)
+let ordersTotal = document.getElementById("orders-total")
 
 function increment() {
     count += 1

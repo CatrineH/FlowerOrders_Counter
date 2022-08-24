38a81de465ad57ed62bf8c52516ee50122117b42
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 23:45:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,16 +11,21 @@ function increment() {
 
 function decrement() {
     count -= 1
-    canceledEl.textContent = count
+//     canceledEl.textContent = count
+    countEl.innerText = count
 }
 
 function save() {
-    let countStr = count + " , "
-    let countStrDe = count - " , "
-    saveEl.textContent += countStr
-    canceledEl.textContent -= countStrDe
-    countEl.textContent = 0
-    count = 0
+//     let countStr = count + " , "
+//     let countStrDe = count - " , "
+//     saveEl.textContent += countStr
+//     canceledEl.textContent -= countStrDe
+//     countEl.textContent = 0
+//     count = 0
+    
+    saveEl.innerText = count;
+    count = 0;
+    countEl.innerText = count;
 }
 
 

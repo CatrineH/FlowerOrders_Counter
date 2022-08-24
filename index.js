dbe3d7f25f746e98091b79e8876ac88a66d00b50
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 23:17:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ let count = 0
 let saveEl = document.getElementById("save-el")
 let canceledEl = document.getElementById("canceled-el")
 
-
-
 function increment() {
     count += 1
     countEl.textContent = count

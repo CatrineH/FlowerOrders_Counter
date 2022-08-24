5a6ab3e7c64d80d4abf11bc0d05ca069b3bab725
Catrine <70755662+CatrineH@users.noreply.github.com>
2022-08-24 21:13:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let saveEl = document.getElementById("save-el")
+let canceledEl = document.getElementById("canceled-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
@@ -7,9 +8,15 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement() {
+    count -= 1
+    canceledEl.textContent = count
+}
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    canceledEl.textContent -= countStr
     countEl.textContent = 0
     count = 0
 }
